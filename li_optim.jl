f8350055bcd08f27e65c803b5a0cdc7058919b35
Richert <richgast.90@gmail.com>
2022-03-18 13:15:37 -0500

    - updated scripts for the lieaky integrator example

--- li_optim.jl
+++ li_optim.jl
@@ -1,8 +1,12 @@
-using LinearAlgebra, DifferentialEquations, NPZ, Plots, GalacticOptim, BlackBoxOptim, Base.Threads
+using Distributed
 
-gr()
+addprocs(3)
 
-function interp(x_new, x, y)
+@everywhere using LinearAlgebra, DifferentialEquations, NPZ, Plots, GalacticOptim, BlackBoxOptim, Base.Threads
+
+@everywhere gr()
+
+@everywhere function interp(x_new, x, y)
     idx = argmin(abs.(x.-x_new))
     if x[idx] > x_new
         i1, i2 = max(idx-1, 1), idx
@@ -13,7 +17,7 @@ function interp(x_new, x, y)
 end
 
 
-function li_eval(t,y,dy,r0,tau,source_idx,k_d1,k_d2,k_d3,k_d4,k_d5,k_d6,k_d7,k_d8,k_d9,k_d10,k_d11,u_timed_input,u_input,time,weight,weight_0)
+@everywhere function li_eval(t,y,dy,r0,tau,source_idx,k_d1,k_d2,k_d3,k_d4,k_d5,k_d6,k_d7,k_d8,k_d9,k_d10,k_d11,u_timed_input,u_input,time,weight,weight_0)
 
 	r = y[1:5]
 	m_d1 = y[6:30]
@@ -51,35 +55,35 @@ function li_eval(t,y,dy,r0,tau,source_idx,k_d1,k_d2,k_d3,k_d4,k_d5,k_d6,k_d7,k_d
 end
 
 
-function l2_loss(x, y)
+@everywhere function l2_loss(x, y)
 	diff = x .- y
 	return sqrt(sum(sum(diff.^2)))
 end
 
 # import function arguments
-vars = npzread("li_params.npz")
-w = npzread("li_fitted_o.npz")["weight"]
-args = "r0,tau,source_idx,k_d1,k_d2,k_d3,k_d4,k_d5,k_d6,k_d7,k_d8,k_d9,k_d10,k_d11,u_timed_input,u_input,time,weight,weight_0"
-args = split(args, ",")
+@everywhere vars = npzread("li_params.npz")
+@everywhere w = npzread("li_fitted_o.npz")["weight"]
+@everywhere args = "r0,tau,source_idx,k_d1,k_d2,k_d3,k_d4,k_d5,k_d6,k_d7,k_d8,k_d9,k_d10,k_d11,u_timed_input,u_input,time,weight,weight_0"
+@everywhere args = split(args, ",")
 
 # basic problem parameters
-T = 100.0
-steps = 10000
-N = 5
-y_shape = size(vars["y"])
+@everywhere T = 100.0
+@everywhere steps = 10000
+@everywhere N = 5
+@everywhere y_shape = size(vars["y"])
 
 # extract pre-fitted connectivity
-idx_r = range(1, N)
-n_par = N^2
-w_0 = zeros(n_par)
-for i=1:N
+@everywhere idx_r = range(1, N)
+@everywhere n_par = N^2
+@everywhere w_0 = zeros(n_par)
+@everywhere for i=1:N
 	idx_c = range((i-1)*N+1,i*N)
 	w_tmp = Diagonal(w[idx_r, idx_c])
 	w_0[idx_c] = Array(w_tmp[abs.(w_tmp) .> 0.0])
 end
 
 # define functions for the parameter update
-function update_connectivity(c)
+@everywhere function update_connectivity(c)
 	c_new = deepcopy(vars["weight"])
 	for i=1:N
 		idx_c = range((i-1)*N+1,i*N)
@@ -88,65 +92,59 @@ function update_connectivity(c)
 	return c_new
 end
 
-function ode_call(du, u, c, t)
+@everywhere function ode_call(du, u, c, t)
 	c_new = update_connectivity(c)
 	return li_eval(t, u, du, [key == "weight" ? c_new : deepcopy(vars[key]) for key in args]...)
 end
 
 # define function call for blackboxoptim
-target = npzread("li_target.npy")
-z = target'
-solver = Tsit5()
-function objective_func(p, p_tmp)
+@everywhere target = npzread("li_target.npy")
+@everywhere z = target'
+@everywhere solver = Tsit5()
+@everywhere function objective_func(p)
 	ode = ODEProblem(ode_call, zeros(y_shape), (0.0, T), p)
-	y = Array(DifferentialEquations.solve(ode, solver, saveat=1e-2, reltol=1e-3, abstol=1e-6))
+	y = Array(solve(ode, solver, saveat=1e-2, reltol=1e-3, abstol=1e-6))
 	return l2_loss(y[1:N, 1:steps], z)
 end
 
 # display original connectivity matrix and target signal
-p1 = plot(target)
-p2 = heatmap(vars["weight"])
+@everywhere p1 = plot(target)
+@everywhere p2 = heatmap(vars["weight"])
 
 # define callback function for intermediate plotting
-t0 = time()
-function cb(p, p_tmp)
-	t1 = time()
+@everywhere function cb(p)
 	ode = ODEProblem(ode_call, zeros(y_shape), (0.0, T), p)
-	y = Array(DifferentialEquations.solve(ode, solver, saveat=1e-2, reltol=1e-3, abstol=1e-6))
+	y = Array(solve(ode, solver, saveat=1e-2, reltol=1e-3, abstol=1e-6))
 	p3 = plot(y[1:N, 1:steps]')
 	c_new = update_connectivity(p)
 	p4 = heatmap(c_new)
 	display(plot(p3, p4, p1, p2, layout=(4,1)))
-	print(t1-t0)
-	print("\n")
-	return t1-t0 > 5000 ? true : false
+	return false
 end
 
 # define optimization problem
-f = OptimizationFunction(objective_func)
-lower = [w_0[i] > 0 ? 0.0 : -1.0 for i=1:n_par]
-upper = [w_0[i] > 0 ? 1.0 : 0.0 for i=1:n_par]
-method = BBO_adaptive_de_rand_1_bin_radiuslimited()
-f = GalacticOptim.OptimizationFunction(objective_func)
-prob = GalacticOptim.OptimizationProblem(f, w_0, lb=lower, ub=upper)
+method = :adaptive_de_rand_1_bin_radiuslimited
+opt = bbsetup(objective_func; Method=method, Parameters=w_0, SearchRange=(-1.0, 1.0), NumDimensions=length(w_0), Workers=workers(),
+	MaxSteps=2000, TargetFitness=0.0, FitnessTolerance=1.0, lambda=50, PopulationSize=5000, CallbackFunction=cb, CallbackInterval=1.0)
 
 # perform optimization
-res = GalacticOptim.solve(prob, method, maxiters=1000, cb=cb, PopulationSize=1000, TargetFitness=0.0, FitnessTolerance=1.0, NThreads=Threads.nthreads()-1)
+el = @elapsed res = bboptimize(opt)
 
 # retrieve optimization results
-w_winner = res.u
+w_winner = best_candidate(res)
 C = zeros(size(vars["weight"]))
 for i=1:N
 	idx_c = range((i-1)*N+1,i*N)
 	C[idx_r, idx_c] = Diagonal(w_winner[idx_c])
 end
+f = best_fitness(res)
 
 # simulate signal of the winner
 ode = ODEProblem(ode_call, zeros(y_shape), (0.0, T), w_winner)
-y = Array(DifferentialEquations.solve(ode, solver, saveat=1e-2, reltol=1e-3, abstol=1e-6))[1:N, 1:steps]
+y = Array(solve(ode, solver, saveat=1e-2, reltol=1e-3, abstol=1e-6))[1:N, 1:steps]
 
 # plot the final result
 cb(w_winner, [])
 
 # save data to file
-npzwrite("li_fitted.npz", Dict("weight" => C, "y" => y', "fitness" => res.minimum))
+npzwrite("li_fitted.npz", Dict("weight" => C, "y" => y', "fitness" => f))
